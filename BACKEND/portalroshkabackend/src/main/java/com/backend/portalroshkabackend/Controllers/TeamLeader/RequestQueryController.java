ee8259ad9411c1a9f3e94ed820b78e3794a0fd95
martinovelar <martinovelarseg@gmail.com>
2025-12-16 10:57:33 -0300

    refactor: restaurar arquitectura cqrs en teamleader para optimizar escalabilidad y srp

--- BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Controllers/TeamLeader/RequestQueryController.java
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Controllers/TeamLeader/RequestQueryController.java
@@ -1,52 +1,24 @@
 package com.backend.portalroshkabackend.Controllers.TeamLeader;
 
-import com.backend.portalroshkabackend.DTO.TeamLeader.TeamLeaderDefaultResponse;
 import com.backend.portalroshkabackend.DTO.TeamLeader.request.TeamLeaderRequestResponseDto;
 import com.backend.portalroshkabackend.Services.TeamLeader.IRequestQueryService;
-import com.backend.portalroshkabackend.Services.TeamLeader.IRequestsCommandService;
-import com.backend.portalroshkabackend.notification.NotificationService;
-import com.backend.portalroshkabackend.notification.webSocket.events.NotificarSolicitudAprobadaEvent;
-import com.backend.portalroshkabackend.notification.webSocket.events.NotificarSolicitudRechazadaEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/admin/teamleader")
-public class TeamLeaderRequestController {
+public class RequestQueryController {
 
-    private final IRequestsCommandService requestsTeamLeaderService;
-    private final NotificationService notificationService;
     private final IRequestQueryService requestQueryService;
-
-
-    @PostMapping("/users/requests/{idSolicitud}/accept")
-    public ResponseEntity<TeamLeaderDefaultResponse> acceptRequest(@PathVariable int idSolicitud) {
-
-        TeamLeaderDefaultResponse respuesta = requestsTeamLeaderService.acceptRequest(idSolicitud);
-
-        NotificarSolicitudAprobadaEvent event = new NotificarSolicitudAprobadaEvent();
-        notificationService.sendEvent(event);
-
-        return ResponseEntity.ok(respuesta);
-    }
-
-    @PostMapping("/users/requests/{idSolicitud}/reject")
-    public ResponseEntity<TeamLeaderDefaultResponse> rejectRequest(@PathVariable int idSolicitud) {
-
-        TeamLeaderDefaultResponse respuesta = requestsTeamLeaderService.rejectRequest(idSolicitud);
-
-        NotificarSolicitudRechazadaEvent event = new NotificarSolicitudRechazadaEvent();
-        notificationService.sendEvent(event);
-
-        return ResponseEntity.ok(respuesta);
-    }
-
 
     @GetMapping("/requests/{id}")
     public ResponseEntity<TeamLeaderRequestResponseDto> getSolicitudById(@PathVariable Integer id) {
@@ -88,5 +60,6 @@ public class TeamLeaderRequestController {
         Page<TeamLeaderRequestResponseDto> solicitudes = requestQueryService.getPermissionsRequests(pageRequest);
         return ResponseEntity.ok(solicitudes);
     }
+
 
 }
