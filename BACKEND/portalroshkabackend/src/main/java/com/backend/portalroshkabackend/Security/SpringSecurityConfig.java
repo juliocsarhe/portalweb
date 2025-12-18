2e18fc4b0b42cd7d510dd69e2a12331962cc122a
tmereles56@gmail.com <tmereles56@gmail.com>
2025-12-18 08:59:03 -0300

    Fix: ruta correcta para TL.

--- BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Security/SpringSecurityConfig.java
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Security/SpringSecurityConfig.java
@@ -80,10 +80,10 @@ public class SpringSecurityConfig {
                 .requestMatchers("/api/v1/admin/sysadmin/**").hasAnyAuthority("ROLE_3", "ROLE_5")
 
                 //role_6 - TEAM LIDER - acceso a team lider
-                .requestMatchers("/api/v1/admin/teamleader/**").hasAuthority("ROLE_6")
+                .requestMatchers("/api/v1/teamleader/**").hasAuthority("ROLE_6")
 
                 // ROLE_5 - DIRECTIVO: tiene acceso a TODOS(menos team lider)
-                .requestMatchers("/api/v1/admin/**").hasAnyAuthority("ROLE_5")
+                //.requestMatchers("/api/v1/admin/**").hasAnyAuthority("ROLE_5")
 
                 // Cualquier otra request requiere autenticación
                 .anyRequest().authenticated()
