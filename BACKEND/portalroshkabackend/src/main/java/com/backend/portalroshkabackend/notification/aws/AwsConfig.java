c1c3a371d5cdd1073e0b16356e1e2adcb9984169
eliasb-21 <elias.benittz@gmail.com>
2025-12-19 15:24:36 -0300

    fix: se eliminan imports sin usar

--- BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/notification/aws/AwsConfig.java
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/notification/aws/AwsConfig.java
@@ -3,8 +3,6 @@ package com.backend.portalroshkabackend.notification.aws;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 
