56ed30d35e1d5ed47f01a54402d526acfd3241d9
tmereles56@gmail.com <tmereles56@gmail.com>
2025-12-22 14:22:26 -0300

    Fix: ruta de TL.

--- BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Controllers/TeamLeader/RequestCommandController.java
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Controllers/TeamLeader/RequestCommandController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/admin/teamleader")
+@RequestMapping("/api/v1/teamleader")
 public class RequestCommandController {
 
     private final IRequestsCommandService requestsTeamLeaderService;
